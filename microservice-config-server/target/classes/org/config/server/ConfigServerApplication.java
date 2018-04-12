3d0778e86350d7a0f0aef5aa67aed407e9d33ef4
zhanghaiyu <zhanghaiyu@reignwood.com>
2018-04-12 08:10:06 +0800

    zhy

--- microservice-config-server/target/classes/org/config/server/ConfigServerApplication.java
+++ microservice-config-server/target/classes/org/config/server/ConfigServerApplication.java
@@ -1,7 +1,9 @@
 package org.config.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
